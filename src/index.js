da974210b35be3044c635c8807fe70ea7a62d9a9
snd <snd@iMac-snd.local>
2019-10-23 22:50:51 +0800

    provider to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+// redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
